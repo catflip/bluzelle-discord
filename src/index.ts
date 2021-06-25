8ca1a91ed42ce3bc0fc173103b26622219d93c35
febriano <rinoakbr@gmail.com>
2021-06-25 10:28:17 +0700

    sekarang sudah bisa membedakan antara admin dan user,setelah ini akan dilanjut membuat smua data terhubung ke discord, juga pada commit ini saya memblock dani untuk tes akan saya hapus setelah hackathon selesai

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import * as dotenv from 'dotenv'
 // @ts-ignore
 dotenv.config({path:`${__dirname}/.env.local`})
-import * as Discord from 'discord.js';
-import { helpMessage } from './lib/helpers';
-const client = new Discord.Client();
+import {Client,Message} from 'discord.js';
+import { helpMessageUser,helpMessageAdmin } from './lib/helpers';
+const client = new Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
-client.on('message', message => {
-    const command=message.content.split(" ")
+client.on('message', (message:Message) => {
+    const command:Array<string>=message.content.split(" ")
+    
+    if(message.author.id==="798053928989098014") return;
 	switch(command[1]){
         case "--help":
-            message.channel.send(`\`\`\`${helpMessage}\`\`\``);
+            message.channel.send(helpMessageUser);
+            if(message.member.hasPermission('ADMINISTRATOR')) message.channel.send(helpMessageAdmin);
+        break;
+        case "--consensus-state":
+            message.channel.send(helpMessageUser);
+            if(message.member.hasPermission('ADMINISTRATOR')) message.channel.send(helpMessageAdmin);
         break;
     }
 });
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,32 +1,41 @@
 /**
- *  Show help message in discord channel
+ *  Show help message for user in discord channel
  */
-export const helpMessage:string=`
+export const helpMessageUser:string=`
 usage:
-  @bluzellebot <command>
+  @bluzellebot --url <url> <command> 
 
-command list:
-  --help: to show help info
-  --consensus-state: to see bluzelle network latest consensus state
-  --latest-block-height: to see bluzelle network latest block height
-  --average-block-time: to see bluzelle network average block time
-  --active-validators: to see bluzelle network active validators
-  --online-voting-power: to see bluzelle network online voting power
-  --latest-blocks: to see bluzelle network latest blocks
-  --transactions: to see bluzelle network latest transactions
-  --price: to see bluzelle token latest price
-  --market-cap: to see bluzelle token market cap
-
-command to send data periodically:
-  --set --url <url> --data <data> --period <period>
+**description**:
   - url(optional): url to get bluzelle data, for example client.sentry.testnet.private.bluzelle.com, default to client.sentry.testnet.private.bluzelle.com
+
+  **command list**:
+  **--help**: to show help info
+  **--consensus-state**: to see bluzelle network latest consensus state
+  **--latest-block-height**: to see bluzelle network latest block height
+  **--average-block-time**: to see bluzelle network average block time
+  **--active-validators**: to see bluzelle network active validators
+  **--online-voting-power**: to see bluzelle network online voting power
+  **--latest-blocks**: to see bluzelle network latest blocks
+  **--transactions**: to see bluzelle network latest transactions
+  **--price**: to see bluzelle token latest price
+  **--market-cap**: to see bluzelle token market cap
+`;
+/**
+ *  Show help message for admin in discord channel
+ */
+export const helpMessageAdmin:string=`
+**Admin Command**
+
+**command to send data periodically**:
+\`\`\`--set --data <data> --period <period>\`\`\`
+description:
   - data: data to send to the discord channel separated by comma for example market-cap, price
   - period(optional): interval to send message to specified discord channel, default to second
 
-command to stop sending data periodically:
-  --stop
+**command to stop sending data periodically:**
+  --stop: to stop sending data periodically
 
-data that can be send periodically:
+**data that can be send periodically**:
     - consensus-state: to see bluzelle network latest consensus state
     - latest-block-height: to see bluzelle network latest block height
     - average-block-time: to see bluzelle network average block time
@@ -37,14 +46,11 @@ data that can be send periodically:
     - price: to see bluzelle token latest price
     - market-cap: to see bluzelle token market cap
 
-period:
+**period**:
     - hourly: to send data per hour
     - minutely: to send data per minute
     - secondly: to send data per second
 
-example to send data periodically:
---set --data price,market-cap --period hourly
-
-
-  
+**example to send data periodically**:
+\`\`\`--set --data price,market-cap --period hourly\`\`\`  
 `;
