cc5f151e8af1204df56783b5e5cebb8385186871
febriano <rinoakbr@gmail.com>
2021-06-30 10:14:35 +0700

    menggunakan slash command

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,117 @@
 import * as dotenv from "dotenv";
 // @ts-ignore
 dotenv.config({ path: `${__dirname}/.env.local` });
-import { Client, Message } from "discord.js";
+import { Client, Message,MessageEmbed,ClientApplication, MessageActionRow,MessageButton, MessageSelectMenu } from "discord.js";
 import { helpMessageUser, helpMessageAdmin,consensusStateMessage } from "./lib/helpers";
 import { Api } from "./lib/api";
-const client = new Client();
 
-client.once("ready", () => {
+const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES']});
+
+client.once("ready", async () => {
   console.log("Ready!");
+  
+  // const wok = new WOKCommands(client, {
+  //   // The name of the local folder for your command files
+  //   commandsDir: "commands",
+  //   testServers: [process.env.GUILD_ID],
+  // });
+  // const Guilds = client.guilds.cache.find(guild => guild.id==="603514468915216415");
+  // const { slashCommands } = wok;
+  // await slashCommands.get("603514468915216415")
+  // await slashCommands.delete("858949390042267678","603514468915216415")
+  // await slashCommands.delete("858875578384121876")
+  
 });
 client.on("message", async (message: Message) => {
-  const command: Array<string> = message.content.split(" ");
-  const api = new Api();
-  if (message.author.id === "798053928989098014") return;
-  switch (command[1]) {
-    case "--help":
-      message.channel.send(helpMessageUser);
-      if (message.member.hasPermission("ADMINISTRATOR"))
-        message.channel.send(helpMessageAdmin);
-      break;
-    case "--consensus-state":
-      const data = await api.getConsensusState();
-      message.channel.send(JSON.stringify(data));
-      break;
+  console.log("s")
+  // const a=await (await client.application?.commands.fetch()).size
+  // console.log(a)
+  // const command = ;
+  // console.log(command);
+  // console.log(client.application)
+  if (!client.application?.owner) {
+    await client.application?.fetch();
+    await client.guilds.cache.get((process.env.GUILD_ID as any))?.commands.set([])
   }
+
+	if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner?.id) {
+    
+		const data = {
+			name: 'help',
+			description: 'Get all of the command that you can send to discord bot',
+    };
+
+		const command = await client.guilds.cache.get((process.env.GUILD_ID as any))?.commands.create(data);
+	
+	}
+  // const command: Array<string> = message.content.split(" ");
+  // const api = new Api();
+  if (message.author.id === "798053928989098014") return;
+  // switch (command[1]) {
+  //   case "--help":
+  //     message.channel.send(helpMessageUser);
+  //     if (message.member.hasPermission("ADMINISTRATOR"))
+  //       message.channel.send(helpMessageAdmin);
+  //     break;
+  //   case "--consensus-state":
+  //     const consensusState = await api.getConsensusState();
+  //     message.channel.send(JSON.stringify(consensusState));
+  //     break;
+  //     case "--proposer":
+  //       const proposer = await api.getMoniker();
+  //       message.channel.send(JSON.stringify(proposer));
+  //       break;
+  // }
+});
+client.on('interaction', async interaction => {
+  console.log("S")
+	if (!interaction.isCommand()) return;
+  if (interaction.user.id === "798053928989098014") return;
+switch(interaction.commandName){
+case "help":
+  const embed = {
+    color: 0x0099ff,
+    title: 'Bluzelle bot',
+    url: 'https://bluzelle.com/',
+    description: `A bot that is able to push updates to the Bluzelle Discord channel, reporting relevant statistics gathered from configured networks.
+    it can report stats from bluzelle testnet and mainnet. Stats reported might include things like # of validators, # of blocks, block times, etc.`,
+    thumbnail: {
+      url: 'https://pbs.twimg.com/profile_images/1397885651547090944/yG9RdL1B_400x400.jpg',
+    },
+    fields: [
+      {
+        name: 'Command list',
+        value: 'list of command that can be used :',
+      },
+      {
+        name: '!deploy',
+        value: 'the first time you get this bot as an admin you must issue this command to deploy the slash command',
+      },
+      {
+        name: '/help',
+        value: 'to get list of command',
+      },
+      {
+        name: '/total-validator',
+        value: 'total number of validator',
+      },
+      {
+        name: '/total-block',
+        value: 'total number of block',
+      },
+      {
+        name: '/block-times',
+        value: 'average block times',
+      },
+    ],
+
+    timestamp: new Date(),
+   
+  };
+
+		await interaction.reply({embeds:[embed]});
+  break;
+}
+	
 });
 client.login(process.env.DISCORD_TOKEN);
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,12 @@
 import axios from "axios";
+import { bech32,bech32m } from "bech32";
+import { tmhash } from 'tendermint/lib/hash'
+import { createAddress } from '@tendermint/sig';
+interface PubKey{
+  "@type":string
+   key:string
+ 
+}
 /**
  *  Connect with the bluzelle network API and RPC to get the data
  */
@@ -15,6 +23,10 @@ export class Api {
    *  the api port of bluzelle network url
    */
   private apiPort: number;
+  /**
+   *  get it from console in here https://bigdipper.testnet.private.bluzelle.com/ by console.log(Meteor.settings.public.bech32PrefixConsAddr)
+   */
+  private bech32PrefixConsAddr: string;
   constructor(
     url: string = "client.sentry.testnet.private.bluzelle.com",
     apiPort: number = 1317,
@@ -23,15 +35,17 @@ export class Api {
     this.url = url;
     this.apiPort = apiPort;
     this.rpcPort = rpcPort;
+    this.bech32PrefixConsAddr = "bluzellevalcons";
   }
   /**
    *  get consensus state from the rpc
    */
   async getConsensusState() {
     const url: string = `https://${this.url}:${this.rpcPort}/dump_consensus_state`;
+    console.log(url)
     try {
       let response = await axios.get(url);
-      let consensus =response.data;
+      let consensus = response.data;
       consensus = consensus.result;
       let height = consensus.round_state.height;
       let round = consensus.round_state.round;
@@ -41,12 +55,14 @@ export class Api {
           consensus.round_state.votes[round].prevotes_bit_array.split(" ")[3]
         ) * 100
       );
+      
+      return this.pubkeyToBech32(consensus.round_state.validators.proposer.pub_key,"bluzellevaloper")
       return {
         votingHeight: height,
         votingRound: round,
         votingStep: step,
         votedPower: votedPower,
-        proposerAddress: consensus.round_state.validators.proposer.address,
+        proposerAddress:this.pubkeyToBech32(consensus.round_state.validators.proposer.pub_key,"bluzellevaloper"),
         prevotes: consensus.round_state.votes[round].prevotes,
         precommits: consensus.round_state.votes[round].precommits,
       };
@@ -55,28 +71,83 @@ export class Api {
       console.log(e);
     }
   }
-  private getProposerName(){
-    let validators = []
+  pubkeyToBech32(pubkey:{type:string,value:string}, prefix:string) {
+    let buffer;
+    try {
+      let pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex');
+      buffer = Buffer.alloc(37);
+
+      pubkeyAminoPrefix.copy(buffer, 0)
+      Buffer.from(pubkey.value, 'base64').copy(buffer, pubkeyAminoPrefix.length)
+
+      return bech32.encode(prefix, bech32.toWords(buffer.slice(0,20)))
+    }
+    catch (e){
+        console.log("Error converting from pubkey to bech32: %o\n %o", pubkey, e);
+        return false
+    }
+}
+  /**
+   *  get proposer moniker
+   */
+  public async getProposerMoniker() {
+    let validators = [];
     let page = 0;
     // let nextKey = 0;
     try {
-        let result;
+      let result;
+      do {
+        const url: string = `https://${this.url}:${
+          this.rpcPort
+        }/validators?&page=${++page}&per_page=100`;
+        let response = await axios.get(url);
+        result = response.data.result;
 
-        do {
-            let url = RPC+`/validators?height=${height}&page=${++page}&per_page=100`;
-            let response = HTTP.get(url);
-            result = JSON.parse(response.content).result;
-            // console.log("========= validator result ==========: %o", result)
-            validators = [...validators, ...result.validators];
-                
-            // console.log(validators.length);
-            // console.log(parseInt(result.total));
-        }
-        while (validators.length < parseInt(result.total))
+        validators = [...validators, ...result.validators];
+      } while (validators.length < parseInt(result.total));
+      let tempValidators = {};
+      for (let v in validators) {
+        validators[v].valconsAddress = this.hexToBech32(
+          validators[v].address,
+          this.bech32PrefixConsAddr
+        );
+        tempValidators[validators[v].address] = validators[v];
+      
+      }
+      return tempValidators;
+    } catch (e) {
+      console.log("Getting validator set at height %o: %o", e);
+    }
+  }
+  /**
+   *  method to convert hex to bech32
+   */
+  private hexToBech32(address: string, prefix: string) {
+    let addressBuffer = Buffer.from(address, "hex");
+    return bech32.encode(prefix, bech32.toWords(addressBuffer));
+  }
+ /**
+   *  method to convert hex to bech32
+   */
+  public async getMoniker(pubkey?:string){
+    let validatorSet = new Map();
+    // get latest validator candidate information
 
+    let url = `https://${this.url}:${
+      this.apiPort
+    }/cosmos/staking/v1beta1/validators?status=BOND_STATUS_BONDED&pagination.limit=200&pagination.count_total=true`;
+
+    try{
+        let response = await axios.get(url);
+        let result = response.data.validators;
+        result.forEach((validator) => validatorSet.set(validator.consensus_pubkey.key,validator)  );
     }
     catch(e){
-        console.log("Getting validator set at height %o: %o", height, e)
+        console.log(url);
+        console.log(e);
     }
+    
+    console.log(validatorSet.size)
+  return "validatorSet";
   }
 }
