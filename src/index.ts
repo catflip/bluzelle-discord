f368525070b8c69f71c18c3d5513220c36a3f507
febriano <rinoakbr@gmail.com>
2021-06-30 14:13:11 +0700

    kurang average block time

--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,171 @@
 import * as dotenv from "dotenv";
 // @ts-ignore
 dotenv.config({ path: `${__dirname}/.env.local` });
-import { Client, Message,MessageEmbed,ClientApplication, MessageActionRow,MessageButton, MessageSelectMenu } from "discord.js";
-import { helpMessageUser, helpMessageAdmin,consensusStateMessage } from "./lib/helpers";
+import {
+  Client,
+  Message,
+  MessageEmbed,
+  ClientApplication,
+  MessageActionRow,
+  MessageButton,
+  MessageSelectMenu,
+} from "discord.js";
+import {
+  helpMessageUser,
+  helpMessageAdmin,
+  consensusStateMessage,
+} from "./lib/helpers";
 import { Api } from "./lib/api";
 
-const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES']});
-
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const commandList = [
+  {
+    name: "help",
+    description: "Get all of the command that you can send to discord bot",
+  },
+  {
+    name: "total-validator",
+    description: "total number of validator",
+  },
+  {
+    name: "total-block",
+    description: "total number of block",
+  },
+  {
+    name: "block-times",
+    description: "average block times",
+  },
+];
 client.once("ready", async () => {
   console.log("Ready!");
-  
-  // const wok = new WOKCommands(client, {
-  //   // The name of the local folder for your command files
-  //   commandsDir: "commands",
-  //   testServers: [process.env.GUILD_ID],
-  // });
-  // const Guilds = client.guilds.cache.find(guild => guild.id==="603514468915216415");
-  // const { slashCommands } = wok;
-  // await slashCommands.get("603514468915216415")
-  // await slashCommands.delete("858949390042267678","603514468915216415")
-  // await slashCommands.delete("858875578384121876")
-  
-});
-client.on("message", async (message: Message) => {
-  console.log("s")
-  // const a=await (await client.application?.commands.fetch()).size
-  // console.log(a)
-  // const command = ;
-  // console.log(command);
-  // console.log(client.application)
   if (!client.application?.owner) {
     await client.application?.fetch();
-    await client.guilds.cache.get((process.env.GUILD_ID as any))?.commands.set([])
+    await client.guilds.cache
+      .get(process.env.GUILD_ID as any)
+      ?.commands.set([]);
+    await client.guilds.cache
+      .get(process.env.GUILD_ID as any)
+      ?.commands.set(commandList);
+  }
+});
+client.on("message", async (message: Message) => {
+  if (!client.application?.owner) {
+    await client.application?.fetch();
+    await client.guilds.cache
+      .get(process.env.GUILD_ID as any)
+      ?.commands.set([]);
   }
 
-	if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner?.id) {
-    
-		const data = {
-			name: 'help',
-			description: 'Get all of the command that you can send to discord bot',
-    };
-
-		const command = await client.guilds.cache.get((process.env.GUILD_ID as any))?.commands.create(data);
-	
-	}
-  // const command: Array<string> = message.content.split(" ");
-  // const api = new Api();
-  if (message.author.id === "798053928989098014") return;
-  // switch (command[1]) {
-  //   case "--help":
-  //     message.channel.send(helpMessageUser);
-  //     if (message.member.hasPermission("ADMINISTRATOR"))
-  //       message.channel.send(helpMessageAdmin);
-  //     break;
-  //   case "--consensus-state":
-  //     const consensusState = await api.getConsensusState();
-  //     message.channel.send(JSON.stringify(consensusState));
-  //     break;
-  //     case "--proposer":
-  //       const proposer = await api.getMoniker();
-  //       message.channel.send(JSON.stringify(proposer));
-  //       break;
-  // }
+  if (
+    message.content.toLowerCase() === "!deploy" &&
+    message.author.id === client.application?.owner?.id
+  ) {
+    const command = await client.guilds.cache
+      .get(process.env.GUILD_ID as any)
+      ?.commands.set(commandList);
+  }
 });
-client.on('interaction', async interaction => {
-  console.log("S")
-	if (!interaction.isCommand()) return;
-  if (interaction.user.id === "798053928989098014") return;
-switch(interaction.commandName){
-case "help":
-  const embed = {
-    color: 0x0099ff,
-    title: 'Bluzelle bot',
-    url: 'https://bluzelle.com/',
-    description: `A bot that is able to push updates to the Bluzelle Discord channel, reporting relevant statistics gathered from configured networks.
+client.on("interaction", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const api = new Api();
+  switch (interaction.commandName) {
+    case "total-validator":
+      const totalValidator = await api.getValidator();
+      const embedTotal = {
+        color: 0x0099ff,
+        title: "Bluzelle bot",
+        url: "https://bluzelle.com/",
+        thumbnail: {
+          url: "https://pbs.twimg.com/profile_images/1397885651547090944/yG9RdL1B_400x400.jpg",
+        },
+        fields: [
+          {
+            name: "Active Validators",
+            value: `${
+              totalValidator.filter(
+                (val: any) =>
+                  val.jailed === false && val.status === "BOND_STATUS_BONDED"
+              ).length
+            } out of ${totalValidator.length} validators`,
+          },
+        ],
+
+        timestamp: new Date(),
+      };
+      await interaction.reply({ embeds: [embedTotal] });
+      break;
+    case "total-block":
+      const totalBlock = await api.getLatestBlockHeight();
+      const embedTotalBlock = {
+        color: 0x0099ff,
+        title: "Bluzelle bot",
+        url: "https://bluzelle.com/",
+        thumbnail: {
+          url: "https://pbs.twimg.com/profile_images/1397885651547090944/yG9RdL1B_400x400.jpg",
+        },
+        fields: [
+          {
+            name: "Latest Block Height",
+            value: `${totalBlock}`,
+          },
+        ],
+
+        timestamp: new Date(),
+      };
+      await interaction.reply({ embeds: [embedTotalBlock] });
+      break;
+    case "help":
+      const embed = {
+        color: 0x0099ff,
+        title: "Bluzelle bot",
+        url: "https://bluzelle.com/",
+        description: `A bot that is able to push updates to the Bluzelle Discord channel, reporting relevant statistics gathered from configured networks.
     it can report stats from bluzelle testnet and mainnet. Stats reported might include things like # of validators, # of blocks, block times, etc.`,
-    thumbnail: {
-      url: 'https://pbs.twimg.com/profile_images/1397885651547090944/yG9RdL1B_400x400.jpg',
-    },
-    fields: [
-      {
-        name: 'Command list',
-        value: 'list of command that can be used :',
-      },
-      {
-        name: '!deploy',
-        value: 'the first time you get this bot as an admin you must issue this command to deploy the slash command',
-      },
-      {
-        name: '/help',
-        value: 'to get list of command',
-      },
-      {
-        name: '/total-validator',
-        value: 'total number of validator',
-      },
-      {
-        name: '/total-block',
-        value: 'total number of block',
-      },
-      {
-        name: '/block-times',
-        value: 'average block times',
-      },
-    ],
+        thumbnail: {
+          url: "https://pbs.twimg.com/profile_images/1397885651547090944/yG9RdL1B_400x400.jpg",
+        },
+        fields: [
+          {
+            name: "Command list",
+            value: "list of command that can be used :",
+          },
+          {
+            name: "!deploy",
+            value:
+              "the first time you get this bot as an admin you must issue this command to deploy the slash command",
+          },
+          {
+            name: "/help",
+            value: "to get list of command",
+          },
+          {
+            name: "/total-validator",
+            value: "total number of validator",
+          },
+          {
+            name: "/total-block",
+            value: "total number of block",
+          },
+          {
+            name: "/block-times",
+            value: "average block times",
+          },
+        ],
 
-    timestamp: new Date(),
-   
-  };
-
-		await interaction.reply({embeds:[embed]});
-  break;
-}
-	
+        timestamp: new Date(),
+      };
+      const row = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setLabel("INVITE")
+          .setStyle("LINK")
+          .setURL("https://google.com"),
+        new MessageButton()
+          .setLabel("COMMAND LIST")
+          .setStyle("LINK")
+          .setURL("https://google.com")
+      );
+      await interaction.reply({ embeds: [embed], components: [row] });
+      break;
+  }
 });
 client.login(process.env.DISCORD_TOKEN);
