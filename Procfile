a80d419b08d5ccbb3759ff75f3586eeb8f5f76a5
febriano <rinoakbr@gmail.com>
2021-07-07 23:50:58 +0700

    build.sh

--- Procfile
+++
@@ -1,3 +0,0 @@
-release: ./build.sh
-web: cd discord-landing && npm start
-worker:ts-node src/index.ts
--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
-npm install
-cd discord-landing && npm install
+sudo apt update
+sudo apt install docker.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+git clone https://github.com/spiritbro1/bluezelle-discord.git
+cd bluezelle-discord
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install stable
+# npm install
+# cd discord-landing && npm install
